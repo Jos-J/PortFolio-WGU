bcd5ed453f261589c005d2016fc5dd58d5395ecb
JJ <joshua.james918@gmail.com>
2025-11-01 20:04:51 -0500

    renamed index to cover

---
+++ script/main.js
@@ -0,0 +1,7 @@
+// Display current date
+const dateDiv = document.getElementById("current-date");
+const today = new Date();
+dateDiv.textContent = "Current date: " + today.toDateString();
+
+// Write a message to the console
+console.log("Resume page loaded successfully!");
